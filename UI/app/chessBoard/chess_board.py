2c3feb4847555e3a399ba8ca8371ee9df30f83b5
Parit Rajput <paritraj1234@gmail.com>
2026-07-26 00:47:06 +0530

    added board painting and missing piece asset

--- UI/app/chessBoard/chess_board.py
+++ UI/app/chessBoard/chess_board.py
@@ -1,5 +1,3 @@
-
-
 class ChessBoard:
     def __init__(self):
         self.board = [["bR","bN","bB","bQ","bK","bB","bN","bR"],
@@ -13,3 +11,20 @@ class ChessBoard:
         
         self.whiteToMove = True
         self.moveLogs = []
+
+
+class MakeMoves:
+
+    rankToRows = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
+    rowsToRank = {"7":"1", "6":"2", "5":"3", "4":"4", "3":"5", "2":"6", "1":"7", "0":"8"}
+    fileToCols = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
+    colsToFile = {"0":"a", "1":"b", "2":"c", "3":"d", "4":"e", "5":"f", "6":"g", "7":"h"}
+
+    def __init__(self,startpos, endpos, board):
+        self.startRow = startpos[0]
+        self.startCol = startpos[1]
+        self.endRow = endpos[0]
+        self.endCol = endpos[1]
+
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
--- UI/app/main.py
+++ UI/app/main.py
@@ -1,18 +1,78 @@
+from pathlib import Path
+
 import pygame as pyg
-from chessBoard import chessBoard
+
+from chessBoard.chess_board import ChessBoard
 
 
 WIDTH = HEIGHT = 512    # Dimensions of the rendered chess board
 SQU_WIDTH = SQU_HEIGHT = 64     # Dimensions of each square in the board
-BOARD_DIM = 8
+DIM = 8
+MAX_FPS = 60
 
 BOARD = {}
 
 def loadAssets():
     pieces = ["bp", "bR" , "bN", "bB", "bQ", "bK", "wp" , "wR" , "wN", "wB", "wQ", "wK"]    #List of pieces 
-    BOARD[pieces] = pyg.image.load("assets/" + pieces + "png")   #mapping pieces with their respective pngs
+    for piece in pieces:
+        BOARD[piece] = pyg.image.load("assets/" + f"{piece}.png")   #mapping pieces with their respective pngs
 
 
+
+def main():
+    pyg.init()
+    screen = pyg.display.set_mode((WIDTH, HEIGHT))
+    clock = pyg.time.Clock()
+    gamestate = ChessBoard()
+    loadAssets()
+
+    running = True
+    squareSelected = ()  # keeps track of the last square clicked (row, col)
+    squareClicked = []  # keeps track of the last two squares clicked (two tuples)
+
+    while running:
+        for event in pyg.event.get():
+            if event.type == pyg.QUIT:
+                running = False
+            elif event.type == pyg.MOUSEBUTTONDOWN:
+                location = pyg.mouse.get_pos()  # (x, y) location of mouse
+                col = location[0] // SQU_WIDTH
+                row = location[1] // SQU_HEIGHT
+                print(row, col)
+
+                if squareSelected == (row, col):  # the user clicked the same square twice
+                    squareSelected = ()  # deselect
+                    squareClicked = []  # clear clicks
+                else:
+                    squareSelected = (row, col)
+                    squareClicked.append(squareSelected)  # append for both 1st and 2nd clicks
+        
+        drawGameState(screen, gamestate)
+        clock.tick(MAX_FPS)
+        pyg.display.flip()
+
+
+def drawGameState(screen, gamestate):
+    drawBoard(screen)
+    drawPieces(screen, gamestate.board)
+
+
+def drawBoard(screen):
+    colors = [pyg.Color("white"), pyg.Color("gray")]
+    for r in range(DIM):
+        for c in range(DIM):
+            color = colors[((r+c) % 2)]
+            pyg.draw.rect(screen, color, pyg.Rect(c*SQU_WIDTH, r*SQU_HEIGHT, SQU_WIDTH, SQU_HEIGHT))
+
+def drawPieces(screen, board):
+    for r in range(DIM):
+        for c in range(DIM):
+            piece = board[r][c]
+            if piece != "__":
+                screen.blit(BOARD[piece], pyg.Rect(c*SQU_WIDTH, r*SQU_HEIGHT, SQU_WIDTH, SQU_HEIGHT))
+
+if __name__ == "__main__":
+    main()
 
 
 
