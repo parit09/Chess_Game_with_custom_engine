92151d80160d2accf3e177f639e10ccdbb49d7c9
Parit Rajput <paritraj1234@gmail.com>
2026-07-28 00:21:54 +0530

    added pawn takes moved like enpasanet and a skeleto for teh is sqsafe function

--- UI/app/chessBoard/chess_board.py
+++ UI/app/chessBoard/chess_board.py
@@ -20,6 +20,8 @@ class ChessBoard:
         self.hasBlackRooksMoved = [False, False]
         self.hasBlackKingMoved = False
         self.hasWhiteKingMoved = False
+        self.whiteKingPos = (7, 4)
+        self.blackKingPos = (0, 4)
 
     def makeMove(self, move):
         validMove = ((move.pieceMoved in ("bp", "wp") and self.pawnMove(move)) or
@@ -29,8 +31,13 @@ class ChessBoard:
                     (move.pieceMoved in ("bB", "wB") and self.bishopMove(move)) or
                     (move.pieceMoved in ("bR", "wR") and self.rookMove(move)))
         if self.validateTurn(move) and validMove:
+            enPassantMove = self.Enpassant(move)
             self.board[move.startRow][move.startCol] = "__"
             self.board[move.endRow][move.endCol] = move.pieceMoved
+            if enPassantMove and self.whiteToMove:
+                self.board[move.endRow + 1][move.endCol] = "__"
+            elif enPassantMove and not self.whiteToMove:
+                self.board[move.endRow - 1][move.endCol] = "__"
             self.moveLogs.append(move)  # log the move so we can undo it later
             self.whiteToMove = not self.whiteToMove  # swap players
             return True
@@ -100,8 +107,13 @@ class ChessBoard:
                     y = y + dy[i]
     
         return possibleMovesList
-                          
+
+    def isSquareSafe(self, row, col):
+        # Implementation for checking if a square is safe
+        pass
+
     def pawnMove(self, move):
+        if(self.pawnTakes(move)): return True
         if self.validMove(move):
             if (move.pieceMoved == "bp"):
                 if move.startRow == 1 and move.endCol == move.startCol and move.endRow == 3:
@@ -115,6 +127,42 @@ class ChessBoard:
                     return True
 
         return False
+
+    def pawnTakes(self, move):
+        if(self.whiteToMove and move.pieceCaptured[0] == "b" and move.pieceCaptured[1] != "K"): return True
+        elif(not self.whiteToMove and move.pieceCaptured[0] == "w" and move.pieceCaptured[1] != "K"): return True
+        elif(self.Enpassant(move)): return True
+        return False
+
+    def Enpassant(self, move):
+        if len(self.moveLogs) == 0:
+            return False
+
+        lastMove = self.moveLogs[-1]
+
+        if self.whiteToMove:
+            return (
+                move.pieceMoved == "wp" and
+                lastMove.pieceMoved == "bp" and
+                move.startRow == 3 and
+                lastMove.startRow == 1 and
+                lastMove.endRow == 3 and
+                abs(lastMove.endCol - move.startCol) == 1 and
+                move.endRow == 2 and
+                move.endCol == lastMove.endCol
+            )
+
+        return (
+                move.pieceMoved == "bp" and
+                lastMove.pieceMoved == "wp" and
+                move.startRow == 4 and
+                lastMove.startRow == 6 and
+                lastMove.endRow == 4 and
+                abs(lastMove.endCol - move.startCol) == 1 and
+                move.endRow == 5 and
+                move.endCol == lastMove.endCol
+            )
+
 
     def rookMove(self, move):
         if (self.validMove(move) and (move.startRow == move.endRow or move.startCol == move.endCol)
@@ -140,6 +188,11 @@ class ChessBoard:
         return False
 
     def kingMove(self, move):
+        if move.pieceMoved == "wK":
+            self.whiteKingPos = (move.endRow, move.endCol)
+        if move.pieceMoved == "bK":
+            self.blackKingPos = (move.endRow, move.endCol)
+
         if self.validMove(move) :
             diffcol = abs(move.startCol - move.endCol)
             diffrow = abs(move.startRow - move.endRow)
@@ -149,6 +202,28 @@ class ChessBoard:
                 return True
 
         return False
+    
+    def possibleKingMoves(self, move):
+        possibleMovesList = []
+        currRow = move.startRow
+        currCol = move.startCol
+        dx = [1, 1, 1, 0, -1, -1, -1, 0]
+        dy = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+        for i in range (8):
+                x, y = currRow + dx[i], currCol + dy[i] 
+                if (x < 8 and y < 8 and x > -1 and y > -1):
+                    if(self.board[x][y] == "__"):
+                        possibleMovesList.append((x,y))
+                    else:
+                        if(self.whiteToMove):
+                            if(self.board[x][y] in self.blackpieces and self.board[x][y] != "bK"):
+                                possibleMovesList.append((x,y))
+                        else:
+                            if(self.board[x][y] in self.whitepieces and self.board[x][y] != "wK"):
+                                possibleMovesList.append((x,y))
+
+        return possibleMovesList
     
 
 class MakeMoves:
